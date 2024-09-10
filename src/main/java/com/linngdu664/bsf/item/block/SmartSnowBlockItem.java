5221f84adf80cc7f987daa14eeb020a82346faa1
Lukelqchen <lukelqchen@tencent.com>
2024-09-10 16:25:40 +0800

    ic 测试添加隐藏物品提示

--- src/main/java/com/linngdu664/bsf/item/block/SmartSnowBlockItem.java
+++ src/main/java/com/linngdu664/bsf/item/block/SmartSnowBlockItem.java
@@ -1,6 +1,9 @@
 package com.linngdu664.bsf.item.block;
 
+import com.linngdu664.bsf.util.BSFCommonUtil;
+import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.contents.TranslatableContents;
@@ -10,7 +13,9 @@ import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 import org.jetbrains.annotations.NotNull;
 import com.linngdu664.bsf.registry.*;
+import org.lwjgl.glfw.GLFW;
 
+import java.awt.im.InputContext;
 import java.util.List;
 
 public class SmartSnowBlockItem extends BlockItem {
@@ -20,20 +25,24 @@ public class SmartSnowBlockItem extends BlockItem {
 
     @Override
     public void appendHoverText(@NotNull ItemStack stack, @NotNull TooltipContext context, List<Component> tooltipComponents, @NotNull TooltipFlag tooltipFlag) {
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block0.tooltip", null, new Object[0])).withStyle(ChatFormatting.BLUE));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block1.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block2.tooltip", null, new Object[0])).withStyle(ChatFormatting.BLUE));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block3.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block4.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block5.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block6.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block7.tooltip", null, new Object[0])).withStyle(ChatFormatting.BLUE));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block8.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block9.tooltip", null, new Object[0])).withStyle(ChatFormatting.BLUE));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block10.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block11.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block12.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block13.tooltip", null, new Object[0])).withStyle(ChatFormatting.BLUE));
-        tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block14.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
+        if (InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_CONTROL)){
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block0.tooltip", null, new Object[0])).withStyle(ChatFormatting.BLUE));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block1.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block2.tooltip", null, new Object[0])).withStyle(ChatFormatting.BLUE));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block3.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block4.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block5.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block6.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block7.tooltip", null, new Object[0])).withStyle(ChatFormatting.BLUE));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block8.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block9.tooltip", null, new Object[0])).withStyle(ChatFormatting.BLUE));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block10.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block11.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block12.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block13.tooltip", null, new Object[0])).withStyle(ChatFormatting.BLUE));
+            tooltipComponents.add(MutableComponent.create(new TranslatableContents("smart_snow_block14.tooltip", null, new Object[0])).withStyle(ChatFormatting.GRAY));
+        }else{
+            BSFCommonUtil.addTrans(tooltipComponents,"show_detail.tip",ChatFormatting.DARK_GRAY,"Ctrl");
+        }
     }
 }
