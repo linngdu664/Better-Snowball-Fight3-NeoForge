79f9039e802fe403fa00358e34e0ed24f602576a
Linngdu664 <664774652@qq.com>
2024-09-12 19:12:57 +0800

    ic 暂时注释

--- src/main/java/com/linngdu664/bsf/gui/RenderGuiEventHandler.java
+++ src/main/java/com/linngdu664/bsf/gui/RenderGuiEventHandler.java
@@ -22,36 +22,36 @@ import java.util.Map;
 public class RenderGuiEventHandler {
     @SubscribeEvent
     public static void onRenderGui(RenderGuiEvent.Post event) {
-        Minecraft instance = Minecraft.getInstance();
-        if (instance.options.hideGui) {
-            return;
-        }
-        Map<String, Object> varMap = new HashMap<>();
-        Player player = instance.player;
-        ItemStack mainHandItem = player.getMainHandItem();
-        ItemStack offHandItem = player.getOffhandItem();
-        HitResult pick = instance.hitResult;
-        HitResult.Type pickType = pick.getType();
-        float partialTick = event.getPartialTick().getGameTimeDeltaPartialTick(true);
-        GuiGraphics guiGraphics = event.getGuiGraphics();
-        guiGraphics.pose().pushPose();
-        guiGraphics.pose().translate(0F, 0F, 4932F);        // 显示在原版gui的上方
-
-        //gui队列
-        GuiHandler.itemInHandBSFWeapon(guiGraphics, mainHandItem, offHandItem);
-        if (pickType == HitResult.Type.ENTITY) {
-            Entity entity1 = ((EntityHitResult) pick).getEntity();
-            GuiHandler.pickEntityBSFSnowGolem(guiGraphics, entity1, partialTick, varMap);
-            GuiHandler.pickEntityBSFDummy(guiGraphics, entity1);
-        } else if (pickType == HitResult.Type.BLOCK) {
-            BlockEntity blockEntity = player.level().getBlockEntity(((BlockHitResult) pick).getBlockPos());
-            GuiHandler.pickBlockEntityVendingMachine(guiGraphics, blockEntity, mainHandItem, partialTick);
-            GuiHandler.pickBlockEntityZoneController(guiGraphics, blockEntity, partialTick);
-        }
-        GuiHandler.itemInHandSnowGolemModeTweaker(guiGraphics, mainHandItem, offHandItem, varMap);
-        GuiHandler.specialModelText(guiGraphics, varMap);
-
-
-        guiGraphics.pose().popPose();
+//        Minecraft instance = Minecraft.getInstance();
+//        if (instance.options.hideGui) {
+//            return;
+//        }
+//        Map<String, Object> varMap = new HashMap<>();
+//        Player player = instance.player;
+//        ItemStack mainHandItem = player.getMainHandItem();
+//        ItemStack offHandItem = player.getOffhandItem();
+//        HitResult pick = instance.hitResult;
+//        HitResult.Type pickType = pick.getType();
+//        float partialTick = event.getPartialTick().getGameTimeDeltaPartialTick(true);
+//        GuiGraphics guiGraphics = event.getGuiGraphics();
+//        guiGraphics.pose().pushPose();
+//        guiGraphics.pose().translate(0F, 0F, 4932F);        // 显示在原版gui的上方
+//
+//        //gui队列
+//        GuiHandler.itemInHandBSFWeapon(guiGraphics, mainHandItem, offHandItem);
+//        if (pickType == HitResult.Type.ENTITY) {
+//            Entity entity1 = ((EntityHitResult) pick).getEntity();
+//            GuiHandler.pickEntityBSFSnowGolem(guiGraphics, entity1, partialTick, varMap);
+//            GuiHandler.pickEntityBSFDummy(guiGraphics, entity1);
+//        } else if (pickType == HitResult.Type.BLOCK) {
+//            BlockEntity blockEntity = player.level().getBlockEntity(((BlockHitResult) pick).getBlockPos());
+//            GuiHandler.pickBlockEntityVendingMachine(guiGraphics, blockEntity, mainHandItem, partialTick);
+//            GuiHandler.pickBlockEntityZoneController(guiGraphics, blockEntity, partialTick);
+//        }
+//        GuiHandler.itemInHandSnowGolemModeTweaker(guiGraphics, mainHandItem, offHandItem, varMap);
+//        GuiHandler.specialModelText(guiGraphics, varMap);
+//
+//
+//        guiGraphics.pose().popPose();
     }
 }
