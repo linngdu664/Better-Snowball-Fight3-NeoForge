b0ef2ed475a33c4ef1dda4ab35e32bddd828791d
zx1316 <1315862279@qq.com>
2024-09-18 00:38:32 +0800

    牛奶不可消除药效

--- src/main/java/com/linngdu664/bsf/effect/WallhackEffect.java
+++ src/main/java/com/linngdu664/bsf/effect/WallhackEffect.java
@@ -2,9 +2,18 @@ package com.linngdu664.bsf.effect;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.neoforged.neoforge.common.EffectCure;
+
+import java.util.Set;
 
 public class WallhackEffect extends MobEffect {
     public WallhackEffect(MobEffectCategory pCategory, int pColor) {
         super(pCategory, pColor);
+    }
+
+
+    @Override
+    public void fillEffectCures(Set<EffectCure> cures, MobEffectInstance effectInstance) {
     }
 }
