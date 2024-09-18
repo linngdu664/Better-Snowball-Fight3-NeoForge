05db4f52c4173eee7e1dc1f9c12216ecf078b54f
Linngdu664 <664774652@qq.com>
2024-09-18 16:44:51 +0800

    ic u盘使用特效

--- src/main/java/com/linngdu664/bsf/item/misc/SuspiciousUSBFlashDriveItem.java
+++ src/main/java/com/linngdu664/bsf/item/misc/SuspiciousUSBFlashDriveItem.java
@@ -1,6 +1,11 @@
 package com.linngdu664.bsf.item.misc;
 
+import com.linngdu664.bsf.network.to_client.ForwardRaysParticlesPayload;
+import com.linngdu664.bsf.particle.util.BSFParticleType;
+import com.linngdu664.bsf.particle.util.ForwardRaysParticlesParas;
 import com.linngdu664.bsf.registry.EffectRegister;
+import com.linngdu664.bsf.registry.SoundRegister;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -11,6 +16,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.network.PacketDistributor;
 import org.jetbrains.annotations.NotNull;
 
 public class SuspiciousUSBFlashDriveItem extends Item {
@@ -24,10 +31,18 @@ public class SuspiciousUSBFlashDriveItem extends Item {
         player.startUsingItem(usedHand);
         return InteractionResultHolder.consume(itemStack);
     }
+    @Override
+    public void onUseTick(Level level, LivingEntity livingEntity, ItemStack stack, int remainingUseDuration) {
+        if (!level.isClientSide) {
+            Vec3 color = new Vec3(0.5, 1, 0.5);
+            PacketDistributor.sendToPlayersTrackingEntityAndSelf(livingEntity, new ForwardRaysParticlesPayload(new ForwardRaysParticlesParas(livingEntity.getPosition(1).add(-0.5, 0, -0.5), livingEntity.getPosition(1).add(0.5, 1, 0.5), color, color.length(), color.length(), 5), BSFParticleType.SPAWN_SNOW.ordinal()));
+        }
+    }
 
     @Override
     public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity livingEntity) {
         livingEntity.addEffect(new MobEffectInstance(EffectRegister.WALLHACK, 2400, 0));
+        livingEntity.playSound(SoundEvents.BEACON_ACTIVATE, 3.0F, 1.0F);
         stack.consume(1, livingEntity);
         return stack;
     }
@@ -37,6 +52,6 @@ public class SuspiciousUSBFlashDriveItem extends Item {
     }
 
     public @NotNull UseAnim getUseAnimation(@NotNull ItemStack stack) {
-        return UseAnim.BOW;
+        return UseAnim.BLOCK;
     }
 }
