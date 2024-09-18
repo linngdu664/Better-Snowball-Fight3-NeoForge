66378b3f622d38df0d728fa70befa7391ab66eaf
Linngdu664 <664774652@qq.com>
2024-09-18 17:10:32 +0800

    ic u盘tooltip

--- src/main/java/com/linngdu664/bsf/item/misc/SuspiciousUSBFlashDriveItem.java
+++ src/main/java/com/linngdu664/bsf/item/misc/SuspiciousUSBFlashDriveItem.java
@@ -5,20 +5,23 @@ import com.linngdu664.bsf.particle.util.BSFParticleType;
 import com.linngdu664.bsf.particle.util.ForwardRaysParticlesParas;
 import com.linngdu664.bsf.registry.EffectRegister;
 import com.linngdu664.bsf.registry.SoundRegister;
+import com.linngdu664.bsf.util.BSFCommonUtil;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.network.PacketDistributor;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class SuspiciousUSBFlashDriveItem extends Item {
     public SuspiciousUSBFlashDriveItem() {
@@ -53,5 +56,12 @@ public class SuspiciousUSBFlashDriveItem extends Item {
 
     public @NotNull UseAnim getUseAnimation(@NotNull ItemStack stack) {
         return UseAnim.BLOCK;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        BSFCommonUtil.addTrans(tooltipComponents,"suspicious_usb_flash_drive.tooltip", ChatFormatting.GRAY);
+        BSFCommonUtil.addTrans(tooltipComponents,"suspicious_usb_flash_drive1.tooltip", ChatFormatting.GRAY);
+        BSFCommonUtil.addTrans(tooltipComponents,"suspicious_usb_flash_drive2.tooltip", ChatFormatting.DARK_GRAY, Minecraft.getInstance().options.keyUse);
     }
 }
